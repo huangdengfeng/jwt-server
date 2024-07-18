61573201f9f83969ff634288f672370fa2449a74
dfenghuang <dfenghuang@tencent.com>
2024-07-18 10:54:39 +0800

    feat(server): add keepalive param

--- entity/grpc/server/server.go
+++ entity/grpc/server/server.go
@@ -3,13 +3,22 @@ package server
 import (
 	log "github.com/sirupsen/logrus"
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/keepalive"
 	"jwt-server/entity/config"
 	"jwt-server/entity/pb"
 	"net"
+	"time"
 )
 
+var kp = keepalive.ServerParameters{
+	MaxConnectionIdle: 3 * time.Minute, // 如果连接空闲超过这个时间，服务端将关闭连接
+}
+var kep = keepalive.EnforcementPolicy{
+	PermitWithoutStream: true, // 空闲时候发ping ，而不是断开连接
+}
+
 func Start(jwtServer pb.JwtServer) *grpc.Server {
-	var grpcServer = grpc.NewServer(CreateDefaultInterceptor())
+	var grpcServer = grpc.NewServer(grpc.KeepaliveParams(kp), grpc.KeepaliveEnforcementPolicy(kep), CreateDefaultInterceptor())
 	pb.RegisterJwtServer(grpcServer, jwtServer)
 
 	go func() {
